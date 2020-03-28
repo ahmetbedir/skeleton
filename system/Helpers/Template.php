f8daa6268bd6f0ef474effa4d167e2616c06c4ed
Ahmet Bedir <ahmetbedir16@gmail.com>
2020-03-29 02:22:53 +0300

    template cache klasörü storage altına taşında

--- system/Helpers/Template.php
+++ system/Helpers/Template.php
@@ -1,34 +1,35 @@
 <?php
 
+use Windwalker\Edge\Cache\EdgeFileCache as EdgeFileCache;
 use Windwalker\Edge\Edge;
 use Windwalker\Edge\Loader\EdgeFileLoader as EdgeFileLoader;
-use Windwalker\Edge\Cache\EdgeFileCache as EdgeFileCache;
 
-class Template {
-    
-    public function render($file, $data = [], $cache = false){
+class Template
+{
+
+    public function render($file, $data = [], $cache = false)
+    {
         $paths = array(VIEWS_PATH);
-        
+
         $loader = new EdgeFileLoader($paths);
         $loader->addFileExtension('.blade.php');
         $loader->addFileExtension('.edge.php');
-        
-        
-        if($cache === true){
+
+        if ($cache === true) {
             $this->edge = new Edge($loader);
-        }else{
-            $this->edge = new Edge($loader, null, new EdgeFileCache(APP_PATH.'/cache'));
+        } else {
+            $this->edge = new Edge($loader, null, new EdgeFileCache(STORAGE_PATH . '/frameword/view/cache'));
         }
-        
+
         $result = null;
-        
+
         try {
             $result = $this->edge->render($file, $data);
         } catch (Exception $e) {
             $result = null;
         }
-        
+
         return $result;
     }
-    
+
 }
