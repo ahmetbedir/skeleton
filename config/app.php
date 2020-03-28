94cc951ccece0f490426c857c3eb0df52ff6071d
Ahmet Bedir <ahmetbedir16@gmail.com>
2020-03-29 02:12:01 +0300

    kod stil düzeltmeleri

--- config/app.php
+++ config/app.php
@@ -1,29 +1,30 @@
 <?php
+
 return array(
     /*
     |--------------------------------------------------------------------------
     | Uygulama Adı
     |--------------------------------------------------------------------------
     |
-    | Bu değer uygulamanın adını belirtir. Ugulamanın adını belirtmen 
+    | Bu değer uygulamanın adını belirtir. Ugulamanın adını belirtmen
     | framework ve paketler için önemlidir.
     |
-    */
+     */
     'name' => 'BDR MVC Project',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Uygualama Ortamı
     |--------------------------------------------------------------------------
     |
     | Bu değer çalışma ortamını belirtir. Development ve Production olarak
-    | belirtebilirsiniz. Böylece uygulama içerisinde hata ve bilgi mesajları 
+    | belirtebilirsiniz. Böylece uygulama içerisinde hata ve bilgi mesajları
     | development modunda daha detaylı olarak alabilirsiniz.
     |
-    */
+     */
     'env' => 'development',
-    
-     /*
+
+    /*
     |--------------------------------------------------------------------------
     | Uygulama URL
     |--------------------------------------------------------------------------
@@ -31,20 +32,20 @@ return array(
     | Bu değer ile uygulamnızın URLsi belirterek framework içerisinde kullanılan
     | belirtilen urlnin başına otomatik eklenecektir.
     |
-    */
-    'base_url'              => 'http://localhost',
+     */
+    'base_url' => 'http://localhost',
     /*
     |--------------------------------------------------------------------------
     | Uygulama Zaman Dilimi
     |--------------------------------------------------------------------------
     |
-    | Bu değer uygulama içerisinde kullandığınız PHP tarih ve saat 
+    | Bu değer uygulama içerisinde kullandığınız PHP tarih ve saat
     | fonksiyonlarda etki gösterecektir. Bölgenize göre desteklenen biçimler
     | PHP tarafından geri döndürülür.
     |
-    */
+     */
     'timezone' => 'UTC',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Şifreleme Anahtarı
@@ -53,9 +54,9 @@ return array(
     | Bu anahtar 32 karakter olarak belirtilmelidir. Şifreleme işlemlerinde
     | kullanılır ve benzersiz olarak projelerinizde kullanmanız önerilir.
     |
-    */
+     */
     'key' => '',
-    
+
     /*
     |--------------------------------------------------------------------------
     | Uygulama Dili
@@ -63,9 +64,9 @@ return array(
     |
     | Bu değer uygulamnızın dilini belirtir ve PHP fonksiyonlarında etkili olur
     |
-    */
-    'language'              => 'tr',
-    
+     */
+    'language' => 'tr',
+
     /*
     |--------------------------------------------------------------------------
     | Varsayılan Controller
@@ -73,30 +74,30 @@ return array(
     |
     | Bu değer uygulama başlatıldığında varsayılan kontrolcüyü devreye sokar.
     |
-    */
-    'default_controller'    => 'Home',
-    
+     */
+    'default_controller' => 'Home',
+
     /*
     |--------------------------------------------------------------------------
     | Varsayılan Controller Method'u
     |--------------------------------------------------------------------------
     |
-    | Bu değer varsayılan controller başlatıldığında 
+    | Bu değer varsayılan controller başlatıldığında
     | çalıştırılacak methodu belirtir.
     |
-    */
-    'default_method'        => 'index',
-    
+     */
+    'default_method' => 'index',
+
     /*
     |--------------------------------------------------------------------------
     | Varsayılan View
     |--------------------------------------------------------------------------
     |
-    | Bu değer varsayılan view dosyasını ayarlar ve ilk olarak bu view gösterilir. 
+    | Bu değer varsayılan view dosyasını ayarlar ve ilk olarak bu view gösterilir.
     |
-    */
-    'default_view'          => 'home.index',
-    
+     */
+    'default_view' => 'home.index',
+
     /*
     |--------------------------------------------------------------------------
     | Composer
@@ -104,6 +105,6 @@ return array(
     |
     | Bu değer uygulama içerisinde composer kullanılıp kullanılmayacağını belirler.
     |
-    */
-    'composer'              => true,
+     */
+    'composer' => true,
 );
