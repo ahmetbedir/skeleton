d4f07daecf28c4d22a1937e5aa5d8d7838eeca4b
Ahmet Bedir <ahmetbedir16@gmail.com>
2020-03-29 02:20:07 +0300

    storage path tanımlandı

--- index.php
+++ index.php
@@ -2,21 +2,18 @@
 
 ini_set('error_reporting', E_ALL);
 
+// Yol tanımlamaları
 define("ROOT_DIR", __DIR__);
-// System Paths
 define("SYSTEM_PATH", ROOT_DIR . "/system");
 define("CORE_PATH", SYSTEM_PATH . "/Core");
 define("HELPERS_PATH", SYSTEM_PATH . "/Helpers");
-
-// App Paths
 define("APP_PATH", ROOT_DIR . "/app");
 define("CONTROLLERS_PATH", ROOT_DIR . "/app/Controllers");
 define("VIEWS_PATH", ROOT_DIR . "/app/Views");
 define("MODELS_PATH", ROOT_DIR . "/app/Models");
-
-// Config ve Vendor dizinleri
-define("VENDOR_PATH", ROOT_DIR . "/vendor");
 define("CONFIG_PATH", ROOT_DIR . "/config");
+define("STORAGE_PATH", ROOT_DIR . "/storage");
+define("VENDOR_PATH", ROOT_DIR . "/vendor");
 
 // Temel framework dosyalarını dahil et
 require CORE_PATH . "/App.php";
